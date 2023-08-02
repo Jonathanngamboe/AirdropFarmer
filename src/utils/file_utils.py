2ff789050422b1a09a47e37badb413477d878c12
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-08-02 18:05:26 +0200

    Changed load_abi to load_json

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -2,7 +2,7 @@
 import json
 import os
 
-def load_abi(file_path):
+def load_json(file_path):
     base_dir = os.path.dirname(os.path.abspath(__file__))  # get the directory of the current file
     abs_file_path = os.path.join(base_dir, file_path)  # join it with the relative path
     with open(abs_file_path, 'r') as f:
