11693ec8109f479cadc77b9c24db03af2e704335
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2023-08-23 12:28:23 +0200

    Add new networks

--- src/networks.js
+++ src/networks.js
@@ -1,20 +1,60 @@
 export const networkParams = {
-  "0x63564c40": {
-    chainId: "0x63564c40",
-    rpcUrls: ["https://api.harmony.one"],
-    chainName: "Harmony Mainnet",
-    nativeCurrency: { name: "ONE", decimals: 18, symbol: "ONE" },
-    blockExplorerUrls: ["https://explorer.harmony.one"],
-    iconUrls: ["https://harmonynews.one/wp-content/uploads/2019/11/slfdjs.png"]
+  "0x1": {
+    chainId: "0x1",
+    rpcUrls: ["https://eth.llamarpc.com"],
+    chainName: "Ethereum Mainnet",
+    nativeCurrency: { name: "ETHER", decimals: 18, symbol: "ETH" },
+    blockExplorerUrls: ["https://etherscan.io"],
+    iconUrls: ["https://ethereum.org/static/eth-diamond-purple.svg"]
   },
-  "0xa4ec": {
-    chainId: "0xa4ec",
-    rpcUrls: ["https://forno.celo.org"],
-    chainName: "Celo Mainnet",
-    nativeCurrency: { name: "CELO", decimals: 18, symbol: "CELO" },
-    blockExplorerUrl: ["https://explorer.celo.org"],
+  "0xa4b1": {
+    chainId: "0xa4b1",
+    rpcUrls: ["https://arbitrum-one.publicnode.com"],
+    chainName: "Arbitrum One",
+    nativeCurrency: { name: "ETHER", decimals: 18, symbol: "ETH" },
+    blockExplorerUrl: ["https://arbiscan.io"],
     iconUrls: [
-      "https://celo.org/images/marketplace-icons/icon-celo-CELO-color-f.svg"
+      "https://arbiscan.io/images/svg/brands/arbitrum.svg?v=1.5"
+    ]
+  },
+  "0x2105": {
+    chainId: "0x2105",
+    rpcUrls: ["https://base.meowrpc.com"],
+    chainName: "Base",
+    nativeCurrency: { name: "ETHER", decimals: 18, symbol: "ETH" },
+    blockExplorerUrl: ["https://base.blockscout.com"],
+    iconUrls: [
+      "https://docs.base.org/img/logo_dark.svg"
+    ]
+  },
+  "0xe708": {
+    chainId: "0xe708",
+    rpcUrls: ["https://rpc.linea.build"],
+    chainName: "Linea",
+    nativeCurrency: { name: "ETHER", decimals: 18, symbol: "ETH" },
+    blockExplorerUrl: ["https://lineascan.build"],
+    iconUrls: [
+      "https://lineascan.build/images/logo.svg?v=23.8.3.0"
+    ]
+  },
+  "0x144": {
+    chainId: "0x144",
+    rpcUrls: ["https://mainnet.era.zksync.io"],
+    chainName: "zkSync Era Mainnet",
+    nativeCurrency: { name: "ETHER", decimals: 18, symbol: "ETH" },
+    blockExplorerUrl: ["https://explorer.zksync.io"],
+    iconUrls: [
+      "https://era.zksync.io/docs/era-dark.svg"
+    ]
+  },
+  "0x44d": {
+    chainId: "0x44d",
+    rpcUrls: ["https://zkevm-rpc.com"],
+    chainName: "Polygon zkEVM",
+    nativeCurrency: { name: "ETHER", decimals: 18, symbol: "ETH" },
+    blockExplorerUrl: ["https://zkevm.polygonscan.com"],
+    iconUrls: [
+      "https://zkevm.polygonscan.com/images/logo.svg?v=23.8.3.0"
     ]
   }
 };
